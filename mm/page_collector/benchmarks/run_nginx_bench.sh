38c0d23a4c00c449ecba8ce7161d1e43e7e58dbe
Adrian Almeida <adrianalmeida417@gmail.com>
2026-09-25 17:33:19 -0300

    test: Increasing benchs time

--- mm/page_collector/benchmarks/run_nginx_bench.sh
+++ mm/page_collector/benchmarks/run_nginx_bench.sh
@@ -8,7 +8,7 @@ sudo systemctl start nginx
 sleep 1
 
 echo "[+] Executing siege (HTTP load test)..."
-siege -c 50 -t 3m http://127.0.0.1:80 |
+siege -c 50 -t 5m http://127.0.0.1:80 |
 	tee "$LOGDIR/nginx_bench.log"
 
 sudo systemctl stop nginx
--- mm/page_collector/benchmarks/run_stress_bench.sh
+++ mm/page_collector/benchmarks/run_stress_bench.sh
@@ -4,7 +4,7 @@ LOGDIR="./bench_logs"
 mkdir -p "$LOGDIR"
 
 echo "[+] Starting stress-ng to cause page faults..."
-stress-ng --vm 8 --vm-bytes 8G --vm-method all --timeout 180s \
+stress-ng --vm 8 --vm-bytes 8G --vm-method all --timeout 300s \
 	--verify --metrics-brief | tee "$LOGDIR/stress-ng.log"
 
 echo "[✓] Stress-ng finished!"
